5b8b5852a8f9339033443c5f7a99449387ec0da5
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-06-29 19:34:53 +0200

    prepare for github

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const serveStatic = require("serve-static");
 
-app = express();
+const app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
